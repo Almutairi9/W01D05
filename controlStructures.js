0a4aa7e23f37101f22ac44fdb696bfbf7cc8f187
Rawan Almutairi <rawan.fahad@tuwaiq.edu.sa>
2021-10-14 12:59:51 +0300

    solev second task

---
+++ controlStructures.js
@@ -0,0 +1,14 @@
+
+let num1 = 12 ;
+let num2 = 5;
+let num3 = 12 ;
+let num = num1  num2 ;
+
+if (num1 > num2) 
+    {  console.log( `The largest Number is ${num1}`);
+         }
+  else {
+      console.log( `The largest Number is ${num2}`);
+       }
+       
+
---
+++ myVariables.js
@@ -0,0 +1,16 @@
+
+let language= "JavaScript";
+let score = "10 "; 
+
+console.log( `Score og the language is  ${language}  ${score}`); 
+
+let length = 5 ;
+let width =  8; 
+let area = length * width 
+
+console.log( `The area of the rectangle is ${area}`);
+
+let num1 = 12 ;
+let num2 = 5;
+let remainder = num1 / num2 
+console.log(`the remainder is ${remainder}`);
